5e5a23a959a1a7ce4693ad7d72f94b58483e3d62
Brirro <cleverbrirro@gmail.com>
2019-08-02 18:48:38 -0400

    Help

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -6,10 +6,39 @@ public class Arrow : MonoBehaviour
 {
     public float speed = 1f;
     public Rigidbody2D rb;
+    public float slowDown = 5f;
 
-    // Use this for initialization
     void Start()
     {
         rb.velocity = transform.right * speed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        speed = 0f;
+    }
+    private void Update()
+    {
+        slowDown -= Time.deltaTime;
+        if(slowDown <= 0)
+        {
+            speed = 0f;
+            rb.velocity = transform.right * speed;
+        } else if (slowDown <= 1)
+        {
+            speed = 1f;
+            rb.velocity = transform.right;
+        } else if (slowDown <= 2)
+        {
+            speed = 2f;
+            rb.velocity = transform.right / speed;
+        } else if (slowDown <= 3)
+        {
+            speed = 3f;
+            rb.velocity = transform.right / speed;
+        } else if (slowDown <= 4)
+        {
+            speed = 4f;
+            rb.velocity = transform.right / speed;
+        }
     }
 }
