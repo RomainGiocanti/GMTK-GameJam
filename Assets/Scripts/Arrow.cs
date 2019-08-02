c136351dbbc9346ba2e7b7cb8f2ff1b4fe5f6b4f
Brirro <cleverbrirro@gmail.com>
2019-08-02 18:09:50 -0400

    Added a weapon controller, and a arrow thing, it will need edits

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public float speed = 1f;
+    public Rigidbody2D rb;
+    public float lifetime;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        Invoke("DestroyProjectile", lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D Hitinfo)
+    {
+
+        Destroy(gameObject);
+    }
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject arrowPrefab;
+    public float offset;
+    public bool hasArrow = true;
+
+    void Update()
+    {
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+            if(hasArrow == true)
+                Shoot();
+            hasArrow = false;
+            }
+    }
+
+    void Shoot()
+    {
+        Instantiate(arrowPrefab, firepoint.position, firepoint.rotation);
+    }
+}
