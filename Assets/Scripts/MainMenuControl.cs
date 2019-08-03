e0b475ea3b9938a8eb8f98a9ce0aab008e93f8bb
dragonrange <design.felipesantos@gmail.com>
2019-08-03 08:35:22 -0300

    TimeBar arrow prefab, sutff

---
+++ Assets/Scripts/MainMenuControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuControl : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject controlsMenu;
+    bool mainMenuBool = true;
+    bool controlsMenuBool = false;
+    
+    public void MainMenuControls()
+    {
+        if(mainMenuBool == true)
+        {
+            mainMenu.SetActive(false);
+            controlsMenu.SetActive(true);
+            controlsMenuBool = true;
+            mainMenuBool = false;
+        }
+        else if(controlsMenuBool == true)
+        {
+            controlsMenu.SetActive(false);
+            mainMenu.SetActive(true);
+            mainMenuBool = true;
+            controlsMenuBool = false;
+        }
+    }
+}
--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -1,49 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootingScript : MonoBehaviour
 {
+    public GameObject shootingCanvas;
+    public Image timerBar;
     public Transform firepoint;
     public GameObject arrowPrefab;
     public float offset;
     public bool hasArrow = true;
     public float buttonHold = 1f;
+    float maximumHoldTime;
     private float buttonHoldDefault;
 
     private void Start()
     {
         buttonHoldDefault = buttonHold;
+        maximumHoldTime = buttonHoldDefault;
     }
     void Update()
     {
-
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
         if (Input.GetButton("Fire1"))
         {
-            if (buttonHold > 0)
+            if (buttonHold >= 0)
             {
                 buttonHold -= Time.deltaTime;
+                timerBar.fillAmount = buttonHold / maximumHoldTime;
             }
 
             if (hasArrow == true && buttonHold <= 0)
             {
+                shootingCanvas.SetActive(true);
                 Shoot();
                 hasArrow = false;
             }
-            if (hasArrow)
-            {
-                Time.timeScale = 0;
-            }
+            
         }
         if (Input.GetButtonUp("Fire1"))
         {
             if (hasArrow == true)
                 Shoot();
             hasArrow = false;
+            shootingCanvas.SetActive(false);
 
         }
 
@@ -51,7 +55,6 @@ public class ShootingScript : MonoBehaviour
         {
             Instantiate(arrowPrefab, firepoint.position, firepoint.rotation);
             buttonHold = buttonHoldDefault;
-            Time.timeScale = 1;
         }
     }
 }
