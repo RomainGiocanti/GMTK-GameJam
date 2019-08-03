60819881a123ab6e7587a2bb2fab28612e026762
dragonrange <design.felipesantos@gmail.com>
2019-08-03 13:50:38 -0300

    Spike animated

---
+++ Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeScript : MonoBehaviour
+{
+    Animator anim;
+    public float timer;
+    float timerDf;
+
+
+    void Start()
+    {
+        timerDf = timer;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            anim.SetFloat("Spiker", timer);
+        }
+        if(timer <= 0)
+        {
+            anim.SetFloat("Spiker", timerDf);
+            timer = timerDf;
+        }
+    }
+}
