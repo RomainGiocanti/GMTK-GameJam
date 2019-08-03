f7ff3bc8614ce7de454f93049bdbfafebc51fc89
dragonrange <design.felipesantos@gmail.com>
2019-08-02 22:33:53 -0300

    Now there is a state machine for each unity, and they take damage :D

--- Assets/Scripts/LivingBeing.cs
+++ Assets/Scripts/LivingBeing.cs
@@ -5,10 +5,34 @@ using UnityEngine;
 public class LivingBeing : MonoBehaviour
 {
     [Header("Life Basics:")]
+    public UnityType unity;
     public int life;
     [Range(0, 10)]
     public float speed;
     [Range(0, 3)]
     public int damage;
-    
+
+    public enum UnityType //This is the type of the unity
+    {
+        player, enemy, barrel, obj
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (unity == UnityType.player && collision.gameObject.CompareTag("Enemy") ||
+            unity == UnityType.player && collision.gameObject.CompareTag("eHit"))
+        {
+            life -= collision.gameObject.GetComponent<LivingBeing>().damage;
+        }
+
+        if (unity == UnityType.enemy && collision.gameObject.CompareTag("Arrow"))
+        {
+            life -= GameObject.FindGameObjectWithTag("Player").GetComponent<LivingBeing>().damage;
+        }
+
+        if (unity == UnityType.barrel && collision.gameObject.CompareTag("Arrow"))
+        {
+            Destroy(gameObject, 1.5f);
+        }
+    }
 }
