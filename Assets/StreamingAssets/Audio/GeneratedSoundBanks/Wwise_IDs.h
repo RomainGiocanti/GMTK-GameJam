081020b5e49e76c9cc69808ee3ab87cd6bb0f023
Simone Selis <simone.selis@icloud.com>
2019-08-04 17:29:24 +0200

    A lot of changes
    Added events;
    Added States;
    Added Interactive Music;
    Removed junk;

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,11 +13,52 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID MAIN_MUSIC = 63796330U;
+        static const AkUniqueID ARROW_BOUNCE = 1008951467U;
+        static const AkUniqueID ARROW_HIT = 2004922858U;
+        static const AkUniqueID ARROW_IMPACT_SYN = 741190258U;
+        static const AkUniqueID ARROW_PICKUP = 1058425211U;
+        static const AkUniqueID ARROW_SHOT = 4139883651U;
+        static const AkUniqueID ARROW_TENSION = 2733655771U;
+        static const AkUniqueID BLOB_DEATH = 1232017583U;
+        static const AkUniqueID BLOB_DMG = 2103945677U;
+        static const AkUniqueID EYE_CHARGE = 493727331U;
+        static const AkUniqueID EYE_DASH = 722354063U;
+        static const AkUniqueID EYE_DEATH = 145372743U;
+        static const AkUniqueID EYE_FLAP = 96323282U;
+        static const AkUniqueID EYE_PAIN = 4053768479U;
+        static const AkUniqueID FOOTSTEP = 1866025847U;
+        static const AkUniqueID GAMELOAD = 2942839011U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PLAYER_DASH = 2394582229U;
+        static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID PLAYER_HIT = 871813740U;
+        static const AkUniqueID PLAYER_INVINCIBILITY = 4162304824U;
+        static const AkUniqueID TIMESWITCH = 2262834706U;
+        static const AkUniqueID TIMESWITCH_REV = 862919212U;
+        static const AkUniqueID TRAP_SPIKES = 584352822U;
+        static const AkUniqueID TURRET_DEATH = 124877840U;
+        static const AkUniqueID TURRET_DMG = 178345354U;
+        static const AkUniqueID TURRET_FIRE = 2132750258U;
+        static const AkUniqueID TURRET_IDLE = 1602890812U;
+        static const AkUniqueID UI_BACK = 2024222415U;
+        static const AkUniqueID UI_CLICK = 2249769530U;
+        static const AkUniqueID UI_CONF = 4159628516U;
     } // namespace EVENTS
 
     namespace STATES
     {
+        namespace GAMESTATE
+        {
+            static const AkUniqueID GROUP = 4091656514U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAME = 702482391U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID VICTORY = 2716678721U;
+            } // namespace STATE
+        } // namespace GAMESTATE
+
         namespace PLAYERALIVE
         {
             static const AkUniqueID GROUP = 2557321869U;
